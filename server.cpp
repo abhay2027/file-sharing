852d0556dee7b9619eaa6b6f4dd486233d30a5f3
Abhay <95165465+abhay2027@users.noreply.github.com>
2025-11-03 00:35:44 +0530

    Add files via upload

---
+++ server.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <fstream>
+using namespace std;
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+class fileserver
+{
+public:
+    void sendfile(int clientsock, const string &filename)
+    {
+        ifstream file(filename, ios::binary | ios::ate);
+        if (!file)
+        {
+            string msg = "ERROR";
+            send(clientsock, msg.c_str(), msg.size(), 0);
+            cerr << "file not found" << endl;
+            exit(1);
+        }
+        long filesize = file.tellg();
+        file.seekg(0, ios::beg);
+    
+        send(clientsock, &filesize, sizeof(filesize), 0);
+    
+        char buffer[1024];
+        while (!file.eof())
+        {
+            file.read(buffer, sizeof(buffer));
+            send(clientsock, buffer, file.gcount(), 0);
+        }
+        file.close();
+        cout << "file sent: " << filesize << "bytes";
+    }
+};
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        cout << "port no. not provided, program terminated";
+        exit(1);
+    }
+    int sockfd, newsockfd, portno, n;
+    char buffer[255];
+
+    struct sockaddr_in serv_addr, cli_addr;
+    socklen_t clilen;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        error("Error opening socket.");
+    }
+    bzero((char *)&serv_addr, sizeof(serv_addr));
+
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+        error("Binding failed");
+
+    listen(sockfd, 5);
+    clilen = sizeof(cli_addr);
+    newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+    if (newsockfd < 0)
+        error("Error on Accept");
+
+    fileserver serverobj;
+    serverobj.sendfile(newsockfd, "The Metamorphosis.pdf");
+    close(newsockfd);
+    close(sockfd);
+    return 0;
+}
