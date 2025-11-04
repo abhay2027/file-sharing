54d57a083fe304991d90ee961eec8f078d606294
Abhay <95165465+abhay2027@users.noreply.github.com>
2025-11-05 00:38:07 +0530

    Refactor client-server file transfer implementation

--- clientwindows.cpp
+++ clientwindows.cpp
@@ -1,70 +1,68 @@
 #include <iostream>
-#include <stdlib.h>
-#include <string.h>
+#include <fstream>
+#include <cstring>
+#include <cstdlib>
 #include <winsock2.h>
 #include <ws2tcpip.h>
-#include <fstream>
-using namespace std;
 
-#pragma comment(lib, "ws2_32.lib") 
+#pragma comment(lib, "ws2_32.lib")
+using namespace std;
 
 void error(const char *msg)
 {
     cerr << msg << " (" << WSAGetLastError() << ")" << endl;
+    WSACleanup();
     exit(1);
 }
 
-class fileclient
+class fetch
 {
-    char filename[200];
+protected:
     int n;
+    char filename[200];
     char menu[500];
 
 public:
-    void files(SOCKET sock)
+    void getfilename(SOCKET sock)
     {
-        n = recv(sock, menu, 500, 0);
+        n = recv(sock, menu, sizeof(menu), 0);
         if (n <= 0)
         {
-            cerr << "Failed to receive menu" << endl;
-            exit(1);
+            error("Failed to read menu");
         }
         menu[n] = '\0';
         cout << menu;
 
-        memset(filename, 0, sizeof(filename));
-        fgets(filename, sizeof(filename), stdin);
-        filename[strcspn(filename, "\r\n")] = '\0';
-        n = send(sock, filename, strlen(filename), 0);
-        if (n <= 0)
-        {
-            cerr << "Failed to send filename" << endl;
-            exit(1);
-        }
-        cout << filename << endl;
+        cin.getline(filename, 200);
+        send(sock, filename, strlen(filename), 0);
+        cout << "Requested file: " << filename << endl;
     }
+};
 
-    void recievefile(SOCKET sock)
+class recvfile : public fetch
+{
+public:
+    void filerecv(SOCKET sock)
     {
         long filesize;
         int n = recv(sock, (char *)&filesize, sizeof(filesize), 0);
         if (n <= 0)
         {
-            cerr << "Failed to receive file size" << endl;
-            exit(1);
+            error("Failed to receive file size");
         }
 
+        // Check if server sent an error message instead of filesize
         if (strncmp((char *)&filesize, "ERROR", 5) == 0)
         {
-            cerr << "File not found" << endl;
+            cerr << "File not found on server." << endl;
             return;
         }
 
         ofstream outfile(filename, ios::binary);
         if (!outfile)
         {
-            cerr << "Failed to open output file" << endl;
-            exit(1);
+            cerr << "Failed to open output file for writing." << endl;
+            return;
         }
 
         char buffer[1024];
@@ -74,62 +72,88 @@ public:
             n = recv(sock, buffer, sizeof(buffer), 0);
             if (n <= 0)
             {
-                cerr << "File not fully received" << endl;
+                cerr << "File transfer interrupted." << endl;
                 break;
             }
             outfile.write(buffer, n);
             totalReceived += n;
         }
+
         outfile.close();
-        cout << "File received: " << totalReceived << " bytes" << endl;
+        cout << "File received successfully (" << totalReceived << " bytes)" << endl;
+    }
+};
+
+class server
+{
+    int argc;
+    char **argv;
+
+public:
+    server(int c, char *v[])
+    {
+        argc = c;
+        argv = v;
+    }
+
+    void ser()
+    {
+        WSADATA wsa;
+        SOCKET sock;
+        struct sockaddr_in serv_addr;
+        struct hostent *server;
+        int portno;
+
+        if (argc < 3)
+        {
+            cerr << "Usage: " << argv[0] << " <hostname> <port>" << endl;
+            exit(1);
+        }
+
+        // Initialize Winsock
+        if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+        {
+            error("Failed to initialize Winsock");
+        }
+
+        portno = atoi(argv[2]);
+        sock = socket(AF_INET, SOCK_STREAM, 0);
+        if (sock == INVALID_SOCKET)
+        {
+            error("Error opening socket");
+        }
+
+        server = gethostbyname(argv[1]);
+        if (server == NULL)
+        {
+            cerr << "Error: no such host." << endl;
+            WSACleanup();
+            exit(1);
+        }
+
+        memset((char *)&serv_addr, 0, sizeof(serv_addr));
+        serv_addr.sin_family = AF_INET;
+        memcpy((char *)&serv_addr.sin_addr.s_addr, (char *)server->h_addr, server->h_length);
+        serv_addr.sin_port = htons(portno);
+
+        if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+        {
+            error("Connection failed");
+        }
+
+        recvfile receivefile;
+        receivefile.getfilename(sock);
+        receivefile.filerecv(sock);
+
+        closesocket(sock);
+        WSACleanup();
     }
 };
 
 int main(int argc, char *argv[])
 {
-    WSADATA wsaData;
-    SOCKET sockfd;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-
-    if (argc < 3)
-    {
-        cerr << "Usage: " << argv[0] << " <hostname> <port>" << endl;
-        exit(1);
-    }
-
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-    {
-        error("WSAStartup failed");
-    }
-
-    int portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == INVALID_SOCKET)
-        error("Error opening socket");
-
-    server = gethostbyname(argv[1]);
-    if (server == NULL)
-    {
-        cerr << "Error: No such host" << endl;
-        WSACleanup();
-        exit(1);
-    }
-
-    memset((char *)&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    memcpy((char *)&serv_addr.sin_addr.s_addr, (char *)server->h_addr, server->h_length);
-    serv_addr.sin_port = htons(portno);
-
-    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-        error("Connection failed");
-
-    fileclient clientobj;
-    clientobj.files(sockfd);
-    clientobj.recievefile(sockfd);
-
-    closesocket(sockfd);
-    WSACleanup();
-
+    server s(argc, argv);
+    s.ser();
     return 0;
 }
+
