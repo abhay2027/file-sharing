887c6edf168c0120a3ee909a80754a6e880a7dac
Abhay <95165465+abhay2027@users.noreply.github.com>
2025-11-10 23:35:40 +0530

    Add files via upload

--- client.cpp
+++ client.cpp
@@ -24,18 +24,29 @@ protected:
 public:
     void getfilename(int sock)
     {
+        bzero(menu, 500);
+        cout << "-------------------------------------------\n";
         n = read(sock, menu, 500);
+        if (n <= 0)
+        {
+            cerr << "Error reading menu\n";
+            return;
+        }
+
         menu[n] = '\0';
         cout << menu;
+        cout << "-------------------------------------------\n";
+        cout << "enter file name to download" << endl;
         bzero(filename, 200);
         fgets(filename, 200, stdin);
         filename[strcspn(filename, "\r\n")] = '\0';
         n = write(sock, filename, strlen(filename));
-        cout << filename;
     }
 };
+
 class recvfile : public fetch
 {
+
 public:
     void filerecv(int sock)
     {
@@ -46,9 +57,10 @@ public:
             cerr << "failed to recieve file size";
             exit(1);
         }
-        if (strncmp((char *)&filesize, "ERROR", 5) == 0)
+        if (filesize == 0)
         {
-            cerr << "file not found";
+            cerr << "File not found or empty on server." << endl;
+            return;
         }
 
         ofstream outfile(filename, ios::binary);
@@ -73,7 +85,160 @@ public:
         cout << "file recieved: " << totalrecieved << " bytes" << endl;
     }
 };
-class server
+
+class upload
+{
+    char filename[80];
+    int n;
+
+public:
+    void selectfile(int sock)
+    {
+        cout << "enter file name" << endl;
+        fgets(filename, 80, stdin);
+        n = write(sock, filename, strlen(filename));
+        filename[strcspn(filename, "\r\n")] = '\0';
+        cout << "Uploaded file: " << filename << endl;
+    }
+
+    void sendfilefun(int sock)
+    {
+
+        ifstream file(filename, ios::binary | ios::ate);
+        if (!file)
+        {
+            string msg = "ERROR";
+            send(sock, msg.c_str(), msg.size(), 0);
+            cerr << "file not found" << endl;
+            return;
+        }
+        cout << filename;
+        long filesize = file.tellg();
+        file.seekg(0, ios::beg);
+        n = send(sock, &filesize, sizeof(filesize), 0);
+
+        char buffer[1024];
+        while (!file.eof())
+        {
+            file.read(buffer, sizeof(buffer));
+            send(sock, buffer, file.gcount(), 0);
+        }
+        file.close();
+        cout << "file sent: " << filesize << " bytes" << endl;
+    }
+};
+
+class menu
+{
+protected:
+    int n;
+    int chose = 0;
+    int chosebytes;
+
+public:
+    void login(int sock)
+    {
+        cout << "1. create account\n"
+                "2. login\n";
+
+        string input;
+        getline(cin, input);
+        chose = stoi(input);
+        chosebytes = htonl(chose);
+        write(sock, (char *)&chosebytes, sizeof(chosebytes));
+        cout << "Choice sent to server.\n";
+    }
+    void choice(int sock)
+    {
+        cout << "select the operation you want to perform\n"
+                "1. download file \n"
+                "2. upload file\n"
+                "Enter a choice :";
+        string input;
+        getline(cin, input);
+        chose = stoi(input);
+        chosebytes = htonl(chose);
+        write(sock, (char *)&chosebytes, sizeof(chosebytes));
+        cout << "Choice sent to server.\n";
+    }
+};
+
+class details
+{
+private:
+    char username[20];
+    int password;
+    string input;
+    int n;
+    int bytes;
+    char errorr[90];
+
+public:
+    void enterdetails(int sock)
+    {
+        cout << "enter username :";
+        fgets(username, 20, stdin);
+        write(sock, (char *)&username, sizeof(username));
+
+        cout << "enter a password :";
+
+        getline(cin, input);
+        password = stoi(input);
+        bytes = htonl(password);
+        write(sock, (char *)&bytes, sizeof(bytes));
+    }
+    void logindetails(int sock)
+    {
+        cout << "enter username :";
+        fgets(username, 20, stdin);
+        write(sock, (char *)&username, sizeof(username));
+        bzero(errorr, sizeof(errorr));
+
+        int n = recv(sock, &errorr, sizeof(errorr), 0);
+        if (n > 0)
+        {
+            errorr[n] = '\0';
+            cout << errorr << std::endl;
+            close(sock);
+            exit(1);
+        }
+        else if (n == 0)
+        {
+            std::cerr << "Server closed connection\n";
+            close(sock);
+        }
+        else
+        {
+            perror("read");
+        }
+        cout << "enter a password :";
+        getline(cin, input);
+        password = stoi(input);
+        bytes = htonl(password);
+        cout << "account created restart\n";
+    }
+    void checkdetails(int sock)
+    {
+        bzero(errorr, sizeof(errorr));
+        int n = read(sock, errorr, sizeof(errorr) - 1);
+        if (n > 0)
+        {
+            cout << errorr << endl;
+
+            if (strstr(errorr, "Invalid username or password"))
+            {
+                close(sock);
+                exit(1);
+            }
+        }
+        else
+        {
+            cerr << "Failed to receive server response." << endl;
+        }
+    }
+};
+
+class server : public menu
 {
     int argc;
     char **argv;
@@ -84,8 +249,6 @@ public:
         argc = c;
         argv = v;
     }
-
-public:
     void ser()
     {
         int sockfd, portno, n;
@@ -114,11 +277,36 @@ public:
             error("connection failed");
 
         recvfile recveivefile;
-        recveivefile.getfilename(sockfd);
-        recveivefile.filerecv(sockfd);
-        close(sockfd);
+        upload uploadfile;
+        menu choose;
+        details logindetails;
+        login(sockfd);
+        if (chose == 1)
+        {
+            logindetails.enterdetails(sockfd);
+            exit(1);
+            close(sockfd);
+        }
+        else if (chose == 2)
+        {
+            logindetails.enterdetails(sockfd);
+            logindetails.checkdetails(sockfd);
+            choice(sockfd);
+        }
+        if (chose == 1)
+        {
+            recveivefile.getfilename(sockfd);
+            recveivefile.filerecv(sockfd);
+            close(sockfd);
+        }
+        if (chose == 2)
+        {
+            uploadfile.selectfile(sockfd);
+            uploadfile.sendfilefun(sockfd);
+        }
     }
 };
+
 int main(int argc, char *argv[])
 {
     server s(argc, argv);
--- server.cpp
+++ server.cpp
@@ -6,13 +6,75 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <fstream>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <algorithm>
+#include <filesystem>
+
 using namespace std;
 void error(const char *msg)
 {
     perror(msg);
     exit(1);
 }
-class fetchfile
+
+class recvfile
+{
+    char filename[80];
+    int n;
+    long filesize;
+
+public:
+    void filerecv(int clientsock, string foldername)
+    {
+        cout << "recieve\n";
+        bzero(filename, 80);
+        n = read(clientsock, filename, 80);
+        if (n < 0)
+        {
+            cerr << "Error reading filename\n";
+            return;
+        }
+        filename[n] = '\0';
+        filename[strcspn(filename, "\r\n")] = '\0';
+        cout << filename << endl;
+        n = recv(clientsock, &filesize, sizeof(filesize), 0);
+        if (n < 0)
+        {
+            cerr << "failed to recieve file size";
+            exit(1);
+        }
+        if (strncmp((char *)&filesize, "ERROR", 5) == 0)
+        {
+            cerr << "file not found";
+        }
+        std::string filepath = "users/" + foldername + "/" + filename;
+        cout << filepath << endl;
+        cout << "received file:  " << filename << endl;
+        ofstream outfile(filepath, ios::binary);
+        if (!outfile)
+        {
+            cerr << "failed to open output file";
+        }
+        char buffer[1024];      //-----
+        long totalrecieved = 0; //-------
+        while (totalrecieved < filesize)
+        {
+            n = recv(clientsock, buffer, sizeof(buffer), 0);
+            if (n < 0)
+            {
+                cerr << "file not recived";
+                exit(1);
+            }
+            outfile.write(buffer, n);
+            totalrecieved += n;
+        }
+        outfile.close();
+        cout << "file recieved: " << totalrecieved << " bytes" << endl;
+    }
+};
+
+class sendfile
 {
 protected:
     int n;
@@ -20,40 +82,33 @@ protected:
     string menu;
 
 public:
-    void file(int clientsock)
+    void filestored(int clientsock, string foldername)
     {
-        menu =
-            "Available files are:\n"
-            "1. Alice in Borderland S03E03 HIN ENG JAP 1080p.mkv\n"
-            "2. C++ Programming Task PDF.pdf\n"
-            "3. The Metamorphosis.pdf\n"
-            "4. white night.pdf\n"
-            "Enter the name of file: ";
-
+        string path = "/home/abhaythakur/project/cpp/server/users/" + foldername;
+        for (const auto &entry : filesystem::directory_iterator(path))
+        {
+            menu += entry.path().filename().string() + "\n";
+        }
+        cout << menu;
         write(clientsock, menu.c_str(), menu.size());
         bzero(filename, 200);
         n = read(clientsock, filename, 200);
         if (n <= 0)
         {
-            cerr << "Error reading file name";
+            cerr << "Error reading file name\n";
             return;
         }
         filename[strcspn(filename, "\r\n")] = '\0';
-        cout << "Requested file: [" << filename << "]" << endl;
+        cout << "Uploaded file:" << filename << endl;
     }
-};
-
-class sendfile : public fetchfile
-{
-public:
-    void sendfilefun(int clientsock)
+    void sendfilefun(int clientsock, string foldername)
     {
-        ifstream file(filename, ios::binary | ios::ate);
+        string path = "/home/abhaythakur/project/cpp/server/users/" + foldername + "/" + string(filename);
+        ifstream file(path, ios::binary | ios::ate);
+        cout << path << endl;
         if (!file)
         {
-            string msg = "ERROR";
-            send(clientsock, msg.c_str(), msg.size(), 0);
-            cerr << "file not found" << endl;
+            cerr << "file not found";
             return;
         }
         long filesize = file.tellg();
@@ -70,7 +125,183 @@ public:
         cout << "file sent: " << filesize << " bytes" << endl;
     }
 };
-class server
+
+class details
+{
+protected:
+    string buffer;
+    char username[20];
+    int password;
+    int n;
+    int count;
+    int byteschoice;
+    string foldername;
+    char storedusr[20];
+    int storedpwd;
+
+private:
+public:
+    void createaccountdetails(int clientsock)
+    {
+        buffer = "enter a unique username :";
+        n = read(clientsock, username, 20);
+        username[strcspn(username, "\n\r")] = '\0';
+
+        cout << username;
+        if (n <= 0)
+        {
+            cerr << "failed to read username";
+            exit(1);
+        }
+
+        ifstream infile("password.txt");
+        string storedusr;
+        int storedpwd;
+        bool exists = false;
+        while (infile >> storedusr >> storedpwd)
+        {
+            if (storedusr == username)
+            {
+                exists = true;
+                break;
+            }
+        }
+        infile.close();
+
+        if (exists)
+        {
+            buffer = "ERROR\n";
+            send(clientsock, buffer.c_str(), buffer.size(),0);
+            return;
+        }
+
+        buffer = "enter a password";
+        n = read(clientsock, (char *)&byteschoice, sizeof(byteschoice));
+        password = ntohl(byteschoice);
+        cout << password << endl;
+
+        ofstream outfile("password.txt", ios::app);
+        outfile << username << " " << password << endl;
+        cout << "pwd stored" << endl;
+        outfile.close();
+    }
+    void enterdetails(int clientsock)
+    {
+        buffer = "enter a unique username :";
+        n = read(clientsock, username, 20);
+        username[strcspn(username, "\n\r")] = '\0';
+
+        cout << username;
+        if (n <= 0)
+        {
+            cerr << "failed to read username";
+            exit(1);
+        }
+
+        buffer = "enter a password";
+        n = read(clientsock, (char *)&byteschoice, sizeof(byteschoice));
+        if (n <= 0)
+        {
+            cerr << "failed to read password";
+            exit(1);
+        }
+        password = ntohl(byteschoice);
+        cout << password << endl;
+    }
+    string returnuser()
+    {
+        return string(username);
+    }
+    void checkdetails(int clientsock)
+    {
+        ifstream infile("password.txt");
+
+        bool match = false;
+        string storedusr;
+        int storedpwd;
+        cout << "enterd " << username << " " << password << endl;
+
+        while (infile >> storedusr >> storedpwd)
+        {
+            cout << "stored " << storedusr << " " << storedpwd << endl;
+            if (storedusr == username && storedpwd == password)
+            {
+                match = true;
+                break;
+            }
+        }
+        if (match)
+        {
+            string msg = "Login successful\n";
+            write(clientsock, msg.c_str(), msg.size());
+        }
+        else
+        {
+            string msg = "Invalid username or password\n";
+            write(clientsock, msg.c_str(), msg.size());
+        }
+    }
+
+    void setusername(string user)
+    {
+        foldername = user;
+    }
+    void createfolder()
+    {
+        mkdir("users", 0777);
+        std::string path = "users/" + foldername;
+        mkdir(path.c_str(), 0777);
+        cout << "folder created";
+    }
+
+    void openfolder()
+    {
+        cout << "folder opened";
+    }
+};
+
+class choice
+{
+protected:
+    int byteschoice = 0;
+    int selectedchoice = 0;
+    int loginchoice;
+    int n;
+
+public:
+    string buffer;
+
+    void login(int clientsock)
+    {
+        buffer = "1. create account\n"
+                 "2. login\n";
+        n = read(clientsock, (char *)&byteschoice, sizeof(byteschoice));
+        if (n <= 0)
+        {
+            cerr << "Failed to read input on login menu\n";
+            close(clientsock);
+        }
+        loginchoice = ntohl(byteschoice);
+    }
+
+    void operation(int clientsock)
+    {
+        buffer = "select the operation you want to perform\n"
+                 "1. download file \n"
+                 "2. upload file\n"
+                 "Enter a choice :";
+        n = read(clientsock, (char *)&byteschoice, sizeof(byteschoice));
+        if (n <= 0)
+        {
+            cerr << "Failed to read choice\n";
+            close(clientsock);
+        }
+        selectedchoice = ntohl(byteschoice);
+        cout << "choice sent " << selectedchoice << endl;
+    }
+};
+
+class server : public choice
 {
     int argc;
     char **argv;
@@ -111,11 +342,12 @@ public:
         if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
             error("Binding failed");
 
-        listen(sockfd, 5);
+        listen(sockfd, 20);
         clilen = sizeof(cli_addr);
+        cout << "Server started on port " << portno << endl;
 
         sendfile filesend;
-        cout << "Server started on port " << portno << endl;
+        recvfile filerecive;
         while (true)
         {
             newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
@@ -125,11 +357,42 @@ public:
                 continue;
             }
             cout << "Client connected" << endl;
-            filesend.file(newsockfd);
 
-            filesend.sendfilefun(newsockfd);
-            cout << "Client disconnected" << endl;
-            close(newsockfd);
+            login(newsockfd);
+            details logindetails;
+
+            if (loginchoice == 1)
+            {
+                logindetails.createaccountdetails(newsockfd);
+                logindetails.returnuser();
+                string username = logindetails.returnuser();
+                logindetails.setusername(username);
+                logindetails.createfolder();
+                close(newsockfd);
+                continue;
+            }
+            else if (loginchoice == 2)
+            {
+                logindetails.enterdetails(newsockfd);
+                logindetails.checkdetails(newsockfd);
+            }
+            operation(newsockfd);
+            string username = logindetails.returnuser();
+            string username_recive_send = logindetails.returnuser();
+
+            if (selectedchoice == 1)
+            {
+                filesend.filestored(newsockfd, username_recive_send);
+                filesend.sendfilefun(newsockfd, username_recive_send);
+                cout << "Client disconnected" << endl;
+                close(newsockfd);
+            }
+            else if (selectedchoice == 2)
+            {
+                filerecive.filerecv(newsockfd, username_recive_send);
+                cout << "Client disconnected" << endl;
+                close(newsockfd);
+            }
         }
 
         close(sockfd);
